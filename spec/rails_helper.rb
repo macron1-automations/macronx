dee0f3020edad32ff1efa46a26cbd48033b9ec3b
Leonid Medovyy <leonid@macron1.io>
2026-06-11 10:05:29 -0700

    setting up devise along with the dashboard controllers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,6 +65,10 @@ RSpec.configure do |config|
   # To enable this behaviour uncomment the line below.
   # config.infer_spec_type_from_file_location!
 
+  # Devise test helpers
+  config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::ControllerHelpers, type: :controller
+
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
